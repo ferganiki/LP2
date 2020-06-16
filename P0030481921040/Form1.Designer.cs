628b4e0affcca9e9b9fb3c956b373a80a4250aa5
ferganiki <56029704+ferganiki@users.noreply.github.com>
2020-06-16 10:52:20 -0300

    Add files via upload

---
+++ P0030481921040/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace P0030481921040
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lstResultado = new System.Windows.Forms.ListBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(12, 12);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(209, 154);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lstResultado
+            // 
+            this.lstResultado.FormattingEnabled = true;
+            this.lstResultado.Location = new System.Drawing.Point(242, 12);
+            this.lstResultado.Name = "lstResultado";
+            this.lstResultado.Size = new System.Drawing.Size(322, 524);
+            this.lstResultado.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(12, 172);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(209, 92);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 544);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lstResultado);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "frm1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lstResultado;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ P0030481921040/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace P0030481921040
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] Vetor = new double[10, 4];
+            double totalmes = 0, totalano = 0;
+            int i, j;
+
+            string auxiliar = "";
+            string valor = "";
+
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    valor = Interaction.InputBox("Digite o total do mês:" + " " + (i + 1) + " " + "da" + " " + "semana:" + " " + (j + 1), "Digitação dos lucros");
+                    if (double.TryParse(valor, out Vetor[i, j]))
+                    {
+                        auxiliar = Vetor[i, j].ToString() + "\n" + auxiliar;
+                        totalmes += Vetor[i, j];
+                        lstResultado.Items.Add("Total mês:" + " " + (i + 1) + " " + "da semana:" + " " + (j + 1) + " " + "R$" + Math.Round(Vetor[i, j], 2));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Valor inválido!");
+                        j--;
+                        continue;
+                    }
+                }
+                lstResultado.Items.Add("Total mês: R$" + " " + Math.Round(totalmes, 2));
+                lstResultado.Items.Add("----------------------------------");
+                totalano += totalmes;
+                totalmes = 0;
+            }
+       
+            lstResultado.Items.Add("Total ano:" + " " + "R$" + Math.Round(totalano, 2));
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstResultado.Items.Clear();
+        }
+    }
+}
+    
